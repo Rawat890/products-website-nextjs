c35b4b45df0b38f498a7118e16f2d5af53046f67
Rawat890 <rohanrawat7508@gmail.com>
2026-09-21 17:33:01 +0530

    feat: update Home component to include HeroSection, FeaturedProducts, and RecentlyLaunchedProducts with loading skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
-import Image from "next/image";
+import FeaturedProducts from "@/components/landing-page/featured-products";
+import HeroSection from "@/components/landing-page/hero-section";
+import RecentlyLaunchedProducts from "@/components/landing-page/recently-launched-products";
+import ProductSkeleton from "@/components/products/product-skeleton";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <h1>Hello next js</h1>
+    <div>
+      <HeroSection />
+
+      <FeaturedProducts />
+
+      <Suspense fallback={<ProductSkeleton />}>
+        <RecentlyLaunchedProducts />
+      </Suspense>
+    </div>
   );
 }
